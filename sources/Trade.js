47015d57dffa28867bdf0f05cea5f9bb9c71bb29
Fabien Lionti <fabien.lionti@gmail.com>
2019-04-06 23:26:44 +0200

    commit

--- sources/Trade.js
+++ sources/Trade.js
@@ -6,4 +6,15 @@ class Trade {
         this.worldTrade_ = new EventEmitter();
         this.timeFactor_ = timeFactor || 1000;
     }
+
+    init() {
+        this.gaiaInterval_ = setInterval(() => {
+
+            if (Math.random() > 0.999)
+            {
+                this.worldTrade_.emit('trade',this.cites);
+            }
+        }, this.timeFactor);
+        this.worldTrade_.on('trade',cites => this.trade(cites));
+    }
 }
