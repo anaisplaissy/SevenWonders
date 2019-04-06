13d68273188a51f4b62d8477d4d16357c5d91bb2
Nerrienet <nerrienetnicolas@yahoo.fr>
2019-04-06 23:39:27 +0200

    le main

---
+++ sources/main.js
@@ -0,0 +1,10 @@
+const city = require("./city");
+const trade = require("./Trade");
+const army = require('./Army');
+
+cite1 = new city.City("paris","macron");
+cite2 = new city.City("lyon","ok");
+
+cite1.giveShit();
+cite2.giveShit();
+
