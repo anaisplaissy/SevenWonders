6275a4d0b94d1cc101a0e73a97b0a52abce7e1bb
anais plaissy <anais.plaissy@live.fr>
2019-04-07 15:41:31 +0200

    Update main

---
+++ sources/main.js
@@ -0,0 +1,28 @@
+const city = require("./City");
+const table = require("./Display");
+const trade = require("./Trade");
+
+cite1 = new city.City("Tatooine","1");
+cite2 = new city.City("Coruscant","2");
+cite3 = new city.City("Kashyyyk","3");
+cite4 = new city.City("Dagobah","4");
+cite5 = new city.City("Endor","5");
+
+cite1.giveShit();
+cite2.giveShit();
+cite3.giveShit();
+cite4.giveShit();
+cite5.giveShit();
+
+echange = new trade.Trade([cite1,cite2,cite3,cite4,cite5]);
+echange.init();
+
+tab1 = new table.Display([cite1,cite2,cite3,cite4,cite5]);
+tab1.init();
+
+
+
+
+
+
+
